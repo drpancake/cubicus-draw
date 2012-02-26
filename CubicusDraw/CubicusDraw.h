8d5eb42cd0c78b79407bb4fa44e7d19b6d0d62e3
James Potter <james.potter@gmail.com>
2012-02-26 17:03:54 +0000

    Constants

---
+++ CubicusDraw/CubicusDraw.h
@@ -0,0 +1,11 @@
+//
+//  CubicusDraw.h
+//  CubicusDraw
+//
+//  Created by James Potter on 26/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+extern NSString *const CD_DAEMON_HOST;
+extern int const CD_DAEMON_PORT;
+extern NSString *const CD_APP_NAME;
---
+++ CubicusDraw/CubicusDraw.m
@@ -0,0 +1,14 @@
+//
+//  CubicusDraw.m
+//  CubicusDraw
+//
+//  Created by James Potter on 26/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CubicusDraw.h"
+
+NSString *const CD_DAEMON_HOST = @"localhost";
+int const CD_DAEMON_PORT = 28739;
+
+NSString *const CD_APP_NAME = @"Cubicus Draw";
