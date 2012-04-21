b585e293b83d95ae41e02aaae6cacf30ee75e8fb
James Potter <james.potter@gmail.com>
2012-04-21 17:36:28 +0100

    Skeleton of paint colour button group

--- CubicusDraw/CDToolsController.h
+++ CubicusDraw/CDToolsController.h
@@ -9,13 +9,21 @@
 #import <Cocoa/Cocoa.h>
 #import "CBShared.h"
 
-@interface CDToolsController : NSWindowController
+enum {
+    CDToolsControllerRed,
+    CDToolsControllerBlue
+} CBToolsControllerElement;
+
+@interface CDToolsController : NSWindowController <CBContextManagerDelegate>
 
 - (id)initWithClient:(CBAppClient *)client;
 
 // Load context managers
 - (void)loadCubicusContexts;
 
+- (IBAction)didClickColor:(id)sender;
+
 @property (nonatomic, strong, readonly) CBAppClient *client;
+@property (nonatomic, readwrite) int selectedColor;
 
 @end
--- CubicusDraw/CDToolsController.m
+++ CubicusDraw/CDToolsController.m
@@ -11,6 +11,7 @@
 @implementation CDToolsController
 
 @synthesize client;
+@synthesize selectedColor;
 
 - (id)initWithClient:(CBAppClient *)theClient
 {
@@ -26,23 +27,67 @@
 {
     [super windowDidLoad];
     [self loadCubicusContexts];
+    
+    // Blue by default
+    self.selectedColor = CDToolsControllerBlue;
 }
 
 - (void)loadCubicusContexts
 {    
-    NSString *button1 = @"{\"id\": 2, \"type\": \"button\", \"label\": \"Button 1\", \"ratio\": 0.33}";
-    NSString *button2 = @"{\"id\": 3, \"type\": \"button\", \"label\": \"Button 2\", \"ratio\": 0.33}";
-    NSString *button3 = @"{\"id\": 4, \"type\": \"button\", \"label\": \"Button 3\", \"ratio\": 0.33}";
-    NSString *hboxString = [NSString stringWithFormat:
-                  @"{\"id\": 1, \"type\": \"hbox\", \"ratio\": 1, \"items\": [%@, %@, %@]}",
-                  button1, button2, button3];
+    NSString *red = [NSString stringWithFormat:
+                     @"{\"id\": %i, \"type\": \"button\", \"label\": \"Red\", \"ratio\": 0.5, \"selected\": true, \"group\": 1}", CDToolsControllerRed];
+    NSString *blue = [NSString stringWithFormat:
+                     @"{\"id\": %i, \"type\": \"button\", \"label\": \"Blue\", \"ratio\": 0.5, \"group\": 1}", CDToolsControllerBlue];
+    
+    NSString *hbox = [NSString stringWithFormat:
+                      @"{\"id\": 1, \"type\": \"hbox\", \"ratio\": 1, \"items\": [%@, %@]}",
+                      red, blue];
+    
     SBJsonParser *parser = [[SBJsonParser alloc] init];
-    CBLayout *toolsLayout = [CBLayout fromJSON:(NSDictionary *)[parser objectWithString:hboxString]];
-    CBContext *context = [[CBContext alloc] initWithID:2 layout:toolsLayout];
+    CBLayout *layout = [CBLayout fromJSON:(NSDictionary *)[parser objectWithString:hbox]];
+    CBContext *context = [[CBContext alloc] initWithID:2 layout:layout];
     
     CBContextManager *manager = [[CBContextManager alloc] initWithContext:context];
+    manager.delegate = self;
 //    [manager wrapView:self.window.contentView];
     [client addContextManager:manager];
+}
+
+#pragma mark -
+#pragma mark Buttons
+
+- (IBAction)didClickColor:(id)sender {
+    NSLog(@"clicked color - b = %@", sender);
+    
+    NSButton *button = (NSButton *)sender;
+    NSUInteger elementID;
+    if ([button.title isEqualToString:@"Red"]) {
+        elementID = CDToolsControllerRed;
+    } else if ([button.title isEqualToString:@"Blue"]) {
+        elementID = CDToolsControllerBlue;
+    }
+    
+    BOOL selected = (button.state == NSOnState);
+    NSDictionary *content = [[NSDictionary alloc] initWithObjectsAndKeys:
+                             [NSNumber numberWithBool:selected], @"selected", nil];
+    CBEvent *event = [[CBEvent alloc] initWithID:elementID content:content];
+    event.contextID = 2;
+    [self.client sendEvent:event];
+    
+//    [button highlight:YES];
+    [button setState:NSOffState];
+}
+
+#pragma mark -
+#pragma mark CBContextManagerDelegate
+
+- (void)manager:(CBContextManager *)manager didReceiveEvent:(CBEvent *)event
+{   
+    if (event.elementID == CDToolsControllerRed) {
+        NSLog(@"got event for red button");
+    } else if (event.elementID == CDToolsControllerBlue) {
+        NSLog(@"got event for blue button");
+    }
 }
 
 @end
